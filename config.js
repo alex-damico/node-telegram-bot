ef963f328cd07da09869b8f0dc12f2b5b42c1f06
Alessandro D'Amico <damico_alex@hotmail.com>
2022-06-14 19:31:22 +0200

    update config

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
+    bot: {
+        token: "",
+        name: "@ArancinDeployBot"
+    },
     help: "Ecco la lista dei comandi a disposizione: ",
     start: "Avvio bot",
     commands: [{
@@ -10,16 +14,6 @@ Siamo un gruppo di professionisti dello sviluppo software che ama ritrovarsi di 
 Sei un* professionist* del software che ama fare il suo lavoro?
 Ti piace condividere pratiche, approcci ed esperienze con altre persone? 
 Allora questa iniziativa fa proprio al caso tuo! 👏
-
-Cos’è ‘sto strano nome?
-Vi starete chiedendo da dove viene questo bizzarro nome... 
-Nel 2021 ci siamo ritrovati a Madonna di Campiglio, per un evento che abbiamo chiamato "Polenta e Deploy". Oltre a mangiar polenta e a fare vie ferrate, abbiamo trascorso due giorni insieme discutendo e confrontandoci su modi migliori per consegnare software di valore ai nostri utenti.
-Questo evento si terrà in una location diversa, immaginate quale possa essere… 😂
-
-Di che cosa si tratta?
-Quello che stiamo cercando di fare è organizzare una un-conference di 2 giorni.
-Una un-conference (un Open Space nel nostro caso) è una "non conferenza" in cui sono gli stessi partecipanti a definire l'agenda. Chiunque partecipi può proporre una sessione (una presentazione, una discussione, una richiesta d'aiuto, etc.), e a seconda degli spazi a disposizione viene di conseguenza definito il programma.
-Verrà comunque spiegato tutto nella mattinata del primo giorno.
         `,
     },
     {
--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const { Telegraf } = require('telegraf')
-const bot = new Telegraf('')
+const { Telegraf } = require('telegraf');
+const bot = new Telegraf('');
 const config = require('./config');
 
 
-bot.start((ctx) => ctx.reply(config.start))
-bot.help((ctx) => ctx.reply(config.help))
+bot.start((ctx) => ctx.reply(config.start));
+bot.help((ctx) => ctx.reply(config.help));
 
 config.commands.forEach(element => {
     bot.command(element.key, (ctx) => {
-        const nameCmd = ctx.update.message.text.slice(1).replace('@ArancinDeployBot', '');
+        const nameCmd = ctx.update.message.text.slice(1).replace(config.bot.name, '');
         const [cmd] = config.commands.filter(x => x.key === nameCmd);
         if (cmd) {
             ctx.reply(cmd.value);
         } else {
-            ctx.reply('Errore: comando non gestito');
+            ctx.reply('Error: command not found');
         }
     })
 });
-  
-bot.launch()
+
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
